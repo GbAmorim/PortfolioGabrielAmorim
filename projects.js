72628e4aeefe42bdcdd7c1bce9981f22e99db37f
Gabriel Amorim <gabrielamorimdeceia@gmail.com>
2025-11-08 00:00:46 -0300

    alterando a aba de projetos e adicionando uma nova section

--- projects.js
+++ projects.js
@@ -1,14 +1,43 @@
-const swiper = new Swiper(".wrapper", {
+// const swiper = new Swiper(".wrapper", {
+//     loop: true,
+//     spaceBetween: 30,
+
+//     autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false,
+//         pauseOnMouseEnter: true,
+//     },
+
+//     //pagination
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         dynamicBullets: true,
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+
+//     breakpoints: {
+//         0: {
+//             slidesPerView: 1,
+//         },
+//         1000: {
+//             slidesPerView: 2,
+//         },
+//         1600: {
+//             slidesPerView: 3,
+//         },
+//     },
+// });
+new Swiper(".card-wrapper", {
     loop: true,
     spaceBetween: 30,
 
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: true,
-    },
-
-    //pagination
+    // Pagination bullets
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -25,10 +54,10 @@ const swiper = new Swiper(".wrapper", {
         0: {
             slidesPerView: 1,
         },
-        1000: {
+        768: {
             slidesPerView: 2,
         },
-        1600: {
+        1024: {
             slidesPerView: 3,
         },
     },
